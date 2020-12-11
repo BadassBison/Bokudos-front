7c5db15d5cb35d948f02e876cb5f15927b5bd4fb
BadassBison <shawn.scott.xd@gmail.com>
2020-12-11 13:55:09 -0600

    Add tslint file and update linting errors

--- src/states/gameState.ts
+++ src/states/gameState.ts
@@ -23,53 +23,53 @@ export class GameState {
             right: false,
             space: false,
             shift: false
-        }
+        };
         this.screenWidth = width;
         this.screenHeight = height;
         this.playerPosition = {
             x: width / 2,
             y: height / 2
-        }
+        };
         console.log('gameState');
     }
 
     parseKey(key: string, pressed: boolean) {
         switch (key) {
-            case "ArrowUp":
-            case "w":
+            case 'ArrowUp':
+            case 'w':
                 if (pressed && !this.keys.up || !pressed && this.keys.up) {
                     this.keys.up = pressed;
                 }
                 break;
 
-            case "ArrowDown":
-            case "s":
+            case 'ArrowDown':
+            case 's':
                 if (pressed && !this.keys.down || !pressed && this.keys.down) {
                     this.keys.down = pressed;
                 }
                 break;
 
-            case "ArrowLeft":
-            case "a":
+            case 'ArrowLeft':
+            case 'a':
                 if (pressed && !this.keys.left || !pressed && this.keys.left) {
                     this.keys.left = pressed;
                 }
                 break;
 
-            case "ArrowRight":
-            case "d":
+            case 'ArrowRight':
+            case 'd':
                 if (pressed && !this.keys.right || !pressed && this.keys.right) {
                     this.keys.right = pressed;
                 }
                 break;
 
-            case " ":
+            case ' ':
                 if (pressed && !this.keys.space || !pressed && this.keys.space) {
                     this.keys.space = pressed;
                 }
                 break;
 
-            case "Shift":
+            case 'Shift':
                 if (pressed && !this.keys.shift || !pressed && this.keys.shift) {
                     this.keys.shift = pressed;
                 }
