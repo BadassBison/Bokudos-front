580f2165191356d73478c0a1c6590bdfbd4cf4d1
codymaki <codymaki@live.com>
2020-12-23 15:18:29 -0600

    Add gravity and jump logic

--- src/objects/ninja.ts
+++ src/objects/ninja.ts
@@ -22,64 +22,43 @@ export class Ninja implements UpdateObject {
 
     updatePosition({ up, right, left, down }: Keys): void {
         // FIXME: this is commented out to add up and down movement
-        // if (!right && !left && !this.jumping) {
-        //     this.currentState = this.movingRight ? AnimationTypes.IDLE_RIGHT : AnimationTypes.IDLE_LEFT;
-        // }
-
-        // FIXME: this is temp for up and down movement for collision testing
-        if (!right && !left && !up && !down) {
+        if (!right && !left && !this.state.jumping) {
             this.state.currentState = this.state.movingRight ? AnimationTypes.IDLE_RIGHT : AnimationTypes.IDLE_LEFT;
         }
 
-        // TODO: Used for jumping
-        // if (up && !this.jumping) {
-        //     this.jumping = true;
-        //     this.currentFrame = -1;
-        //     this.currentState = this.movingRight ? AnimationTypes.JUMP_RIGHT : AnimationTypes.JUMP_LEFT;
-        // }
-
-        // TODO: Used for jumping
-        // if (this.jumping) {
-        //     if (this.currentFrame < 2) {
-        //         this.position.y += .25;
-        //     } else if (this.currentFrame < 4) {
-        //         this.position.y += .125;
-        //     } else if (this.currentFrame >= 8) {
-        //         this.position.y -= .25;
-        //     } else if (this.currentFrame >= 6) {
-        //         this.position.y -= .125;
-        //     }
-        // }
-
-        let velocity = { dx: 0, dy: 0 };
+        let velocity = { dx: 0, dy: this.state.velocity.dy};
+        if (up && !this.state.jumping) {
+            this.state.jumping = true;
+            this.state.currentFrame = -1;
+            this.state.currentState = this.state.movingRight ? AnimationTypes.JUMP_RIGHT : AnimationTypes.JUMP_LEFT;
+            velocity.dy += this.state.jumpSpeed;
+        }
 
         if (right) {
             this.state.movingRight = true;
             if (!this.state.jumping) { this.state.currentState = AnimationTypes.RUN_RIGHT; }
-            velocity.dx += this.state.speed;
+            velocity.dx += this.state.movementSpeed;
         }
 
         if (left) {
             this.state.movingRight = false;
             if (!this.state.jumping) { this.state.currentState = AnimationTypes.RUN_LEFT; }
-            velocity.dx -= this.state.speed;
+            velocity.dx -= this.state.movementSpeed;
         }
 
-        // FIXME: Delete this.state if block after collision detection is fixed
-        if (up) {
-            this.state.movingRight = true;
-            if (!this.state.jumping) { this.state.currentState = AnimationTypes.RUN_RIGHT; }
-            velocity.dy += this.state.speed;
-        }
 
-        // FIXME: Delete this.state if block after collision detection is fixed
-        if (down) {
-            this.state.movingRight = true;
-            if (!this.state.jumping) { this.state.currentState = AnimationTypes.RUN_LEFT; }
-            velocity.dy -= this.state.speed;
+        if(velocity.dy > -this.state.terminalVelocity) {
+            velocity.dy -= this.state.gravity;
         }
 
         const updatedVelocity = CollisionUtilities.collideWithTiles(this.state.hitbox, velocity);
+
+        if(!this.state.jumping && updatedVelocity.dy !== 0) {
+            this.state.jumping = true;
+        } else if(updatedVelocity.dy === 0) {
+            this.state.jumping = false;
+        }
+        this.state.velocity = updatedVelocity;
 
         this.state.position.x += updatedVelocity.dx;
         this.state.position.y += updatedVelocity.dy;
@@ -109,7 +88,6 @@ export class Ninja implements UpdateObject {
                 this.state.currentFrame++;
                 if (this.state.currentFrame >= this.state.framesPerAnimation) {
                     this.state.currentFrame %= this.state.framesPerAnimation;
-                    this.state.jumping = false;
                 }
             } else {
                 this.state.currentFrame = (this.state.currentFrame + 1) % this.state.framesPerAnimation;
