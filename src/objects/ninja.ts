9cf8c889a91eca18bd5ad97dd3736d4c3417698b
codymaki <codymaki@live.com>
2020-12-30 16:17:17 -0600

    Add attack animations

--- src/objects/ninja.ts
+++ src/objects/ninja.ts
@@ -45,6 +45,7 @@ export class Ninja implements UpdateObject {
 
     updateProperties(keys: Keys): void {
         this.updatePosition(keys);
+        this.updateAttackState(keys);
     }
 
     updateAnimation() {
@@ -53,7 +54,17 @@ export class Ninja implements UpdateObject {
         this.draw();
     }
 
-    updatePosition({ up, right, left, down }: Keys): void {
+    updateAttackState({attack}: Keys): void {
+        // indicate that we are starting the attack animation
+        if(attack && !this.state.attacking && !this.state.attackUsed) {
+            this.state.attacking = true;
+            this.state.currentFrame = -1;
+        }
+        // we may want to add some kind of timer to prevent the user from attacking too fast
+        this.state.attackUsed = attack;
+    }
+
+    updatePosition({ up, right, left }: Keys): void {
         const velocity = { dx: 0, dy: this.state.velocity.dy };
         if (up && !this.state.jumping && !this.state.jumpUsed) {
             velocity.dy += this.state.jumpSpeed;
@@ -75,11 +86,15 @@ export class Ninja implements UpdateObject {
         this.state.jumpUsed = up;
 
         // TODO: may want to reconsider how this is being done... This is to center the view on the ninja
-        State.gameState.position = { x: this.state.position.x - State.gameState.gameUnitDimensions.w / 2 + .5, y: this.state.position.y - 5 };
+        State.gameState.position = { x: this.state.position.x - State.gameState.gameUnitDimensions.w / 2, y: this.state.position.y - 5 };
         this.updateHitboxAndCollisionDetectionBoxPositions();
     }
 
     setCurrentAnimationState() {
+        if(this.state.attacking && this.state.currentFrame === this.state.framesPerAnimation-1) {
+            this.state.attacking = false;
+        }
+
         // if the velocity is 0, keep the direction that the character was last facing
         if (this.state.velocity.dx !== 0) {
             this.state.movingRight = this.state.velocity.dx > 0;
@@ -88,26 +103,33 @@ export class Ninja implements UpdateObject {
             this.state.jumping = false;
         } else if (!this.state.jumping) {
             this.state.jumping = true;
-            this.state.currentFrame = -1;
-            this.state.currentState = this.state.movingRight ? AnimationTypes.JUMP_RIGHT : AnimationTypes.JUMP_LEFT;
+            if(this.state.attacking) {
+                this.state.currentState = this.state.movingRight ? AnimationTypes.JUMP_ATTACK_RIGHT : AnimationTypes.JUMP_ATTACK_LEFT;
+            } else {
+                this.state.currentState = this.state.movingRight ? AnimationTypes.JUMP_RIGHT : AnimationTypes.JUMP_LEFT;
+            }
         }
 
         if (!this.state.jumping) {
-            if (this.state.velocity.dx === 0) {
+            if(this.state.attacking) {
+                this.state.currentState = this.state.movingRight ? AnimationTypes.ATTACK_RIGHT : AnimationTypes.ATTACK_LEFT;
+            } else if (this.state.velocity.dx === 0) {
                 this.state.currentState = this.state.movingRight ? AnimationTypes.IDLE_RIGHT : AnimationTypes.IDLE_LEFT;
             } else {
                 this.state.currentState = this.state.movingRight ? AnimationTypes.RUN_RIGHT : AnimationTypes.RUN_LEFT;
             }
+        } else if(this.state.attacking) {
+            this.state.currentState = this.state.movingRight ? AnimationTypes.JUMP_ATTACK_RIGHT : AnimationTypes.JUMP_ATTACK_LEFT;
         }
     }
 
     updateHitboxAndCollisionDetectionBoxPositions() {
         this.state.hitbox.position = {
-            x: this.state.position.x,
+            x: this.state.position.x - this.state.hitbox.dimensions.w/2,
             y: this.state.position.y - this.state.hitboxOffset.h
         };
         this.state.collisionDetectionBox.position = {
-            x: this.state.position.x - 2,
+            x: this.state.position.x - 2 - this.state.hitbox.dimensions.w/2,
             y: this.state.position.y + 2
         };
     }
@@ -117,14 +139,7 @@ export class Ninja implements UpdateObject {
         this.state.frameCount %= this.state.frameDelay;
 
         if (this.state.frameCount === 0) {
-            if (this.state.jumping) {
-                this.state.currentFrame++;
-                if (this.state.currentFrame >= this.state.framesPerAnimation) {
-                    this.state.currentFrame %= this.state.framesPerAnimation;
-                }
-            } else {
-                this.state.currentFrame = (this.state.currentFrame + 1) % this.state.framesPerAnimation;
-            }
+            this.state.currentFrame = (this.state.currentFrame + 1) % this.state.framesPerAnimation;
             this.state.currentImage = this.state.animations.getAnimation(this.state.currentState).getImages()[this.state.currentFrame];
         }
     }
@@ -134,7 +149,7 @@ export class Ninja implements UpdateObject {
         const { w, h } = RenderingUtilities.toScreenDimensions(this.getSize());
 
         State.gameState.canvas.ctx.drawImage(
-            State.ninjaState.currentImage, x, y, w, h
+            State.ninjaState.currentImage, x - w/2, y, w, h
         );
     }
 
