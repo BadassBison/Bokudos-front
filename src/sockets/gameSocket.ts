956aa2964ca847351a378e77f9cf1506527d6f07
codymaki <codymaki@live.com>
2021-02-13 14:39:43 -0600

    Fix initial player load issue

--- src/sockets/gameSocket.ts
+++ src/sockets/gameSocket.ts
@@ -28,13 +28,23 @@ export class GameSocket {
         this.playerDto = playerDto;
         this.webSocket = new WebSocket(`ws://localhost:8082/api/play/${this.gameDto.gameId}`);
         this.webSocket.onopen = (event) => {
+            console.log(event);
             this.setConnected(true);
+            const packet = new OutPacket();
+            packet.playerId = this.playerDto ? this.playerDto.playerId : null;
+            packet.keys = {left: false, attack: false, down: false, right: false, shift: false, up: false};
+
+            const packetString = JSON.stringify(packet);
+            if (this.connected && this.lastPacket !== packetString && packet.playerId) {
+                this.webSocket.send(packetString);
+                this.lastPacket = packetString;
+            }
             console.log('Open: ', event);
         };
         this.webSocket.onmessage = (event) => {
             const serverUpdatePacket: ServerUpdatePacket = JSON.parse(event.data);
 
-            if(serverUpdatePacket.players != null) {
+            if (serverUpdatePacket.players != null) {
                 this.updateAssets(serverUpdatePacket.players);
             }
             if (serverUpdatePacket.enemies != null) {
@@ -49,17 +59,17 @@ export class GameSocket {
 
     updateAssets(assets: Map<string, PositionData>): void {
         new Map(Object.entries(assets)).forEach((value, key) => {
-            if(State.gameState.assetMap == null) {
+            if (State.gameState.assetMap == null) {
                 State.gameState.assetMap = new Map<string, UpdateObject>();
             }
-            if(!State.gameState.assetMap.has(key)) {
+            if (!State.gameState.assetMap.has(key)) {
                 const asset = value.assetType === AssetType.ENEMY ? new Enemy(key) : new Player(key);
                 asset.setPositionData(value);
                 State.gameState.assetMap.set(key, asset);
             } else {
                 State.gameState.assetMap.get(key).setPositionData(value);
             }
-            if(key === this.playerDto.playerId) {
+            if (key === this.playerDto.playerId) {
                 RenderingUtilities.setScreenPositionFromCenter({x: value.x, y: value.y});
             }
         });
